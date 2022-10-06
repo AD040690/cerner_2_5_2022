987db8d5d1992960864faa49bd7a80ba940ae22a
AD040690 <115162627+AD040690@users.noreply.github.com>
2022-10-06 21:38:55 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,24 @@
+package cerner.programmerDay.event;
+
+public class FibonacciSeries{
+    public boolean printFibonacciNumbers(int tremCount) {
+        long currentTerm = 0, nextTerm = 1;
+        try {
+            System.out.println("Fibonacci Series till " + tremCount + " terms:");
+
+            for (int loopCounter = 0; loopCounter < (tremCount - 1); ++loopCounter)
+            {
+                System.out.print(currentTerm + ", ");
+                long possibleNextTerm = currentTerm + nextTerm;
+                currentTerm = nextTerm;
+                nextTerm = possibleNextTerm;
+            }
+            System.out.print(currentTerm + " ... ");
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+        return true;
+    }
+}
