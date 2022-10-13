992106cab9cb64152bf5b001a84220bd02c5c34a
AD040690 <115162627+AD040690@users.noreply.github.com>
2022-10-13 23:25:48 +0530

    Add files via upload

---
+++ SimpleAddition.py
@@ -0,0 +1,10 @@
+# cerner_2tothe5th_2022
+# Store input numbers
+firstNumber = input('Enter first number: ')
+secondNumber = input('Enter second number: ')
+
+# Add two numbers
+sumResult = float(firstNumber) + float(secondNumber)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(firstNumber, secondNumber, sumResult))
